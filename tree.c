d2aa86d7dd87ce92eeebff2e0b6c094c80e079bf
laukik <laukiknirgudkar@gmail.com>
2013-09-22 10:09:49 +0530

    source modified

--- tree.c
+++ tree.c
@@ -3,16 +3,19 @@
 #include "stdlib.h"
 struct tree{
 	int data;
+	struct tree *father;
 	struct tree *lptr;
 	struct tree *rptr;
 };
 typedef struct tree node;
+node *rut;
 node *new(){
 	return (node *)malloc(sizeof(node));
 }
 node *root(int value){            //call at the time of initilisation
 	node *ptr = new();
 	ptr-> data = value;
+	ptr-> father = 0;
 	ptr-> lptr = 0;
 	ptr-> lptr = 0;
 	return ptr;
@@ -24,6 +27,7 @@ void insert(int x, node *ptr){
 		}else{
 			node *temp = new();
 			temp-> data = x;
+			temp-> father = ptr;
 			temp-> lptr = 0;
 			temp-> rptr = 0;
 			ptr-> rptr = temp;
@@ -34,6 +38,7 @@ void insert(int x, node *ptr){
 		}else{
 			node *temp = new();
 			temp-> data = x;
+			temp-> father = ptr;
 			temp-> lptr = 0;
 			temp-> rptr = 0;
 			ptr-> lptr = temp;
@@ -69,6 +74,12 @@ node *search(int x, node *ptr){
 		else return search(x,ptr->lptr);
 	}else return 0;
 }
+node *parent(int x, node *ptr){
+	node *temp;
+	if(temp = search(x,ptr)){
+		return temp-> father;
+	}else return 0; 
+}
 node *lchild(int x, node *ptr){
 	node *temp;
 	if(temp = search(x,ptr)){
@@ -82,10 +93,48 @@ node *rchild(int x, node *ptr){
 	}else return 0;
 }
 int delete(int x, node *ptr){
-
+	node *temp;
+	if(temp = search(x,ptr)){
+		if(temp == ptr){						   /// ROOT
+			node *z = ptr-> rptr;
+			ptr-> rptr-> father = 0;
+			while( z-> lptr) z = z-> lptr;
+			z-> lptr = ptr-> lptr;
+			rut = ptr-> rptr;
+			free(ptr);
+		}else if(ptr-> lptr && ptr-> rptr){		   /// Both child
+			node *z,*temp = ptr-> father;
+			temp-> rptr = ptr-> rptr;
+			ptr-> rptr-> father = temp;
+			z = temp-> rptr;
+			while( z-> lptr) z = z-> lptr;
+			z-> lptr = ptr-> lptr;
+			free(ptr);
+		}else if(ptr-> lptr && !ptr-> rptr){       /// Left child only
+			node *temp = ptr-> father;
+			temp-> lptr = ptr-> lptr;
+			ptr-> lptr-> father = temp;
+			free(ptr);
+		}else if(!ptr-> lptr && ptr-> rptr){	   /// Right child only
+			node *temp = ptr-> father;
+			temp-> rptr = ptr-> rptr;
+			ptr-> rptr-> father = temp;
+			free(ptr);
+		}else{                                     /// No child
+			node *temp = ptr-> father;
+			int k = temp->data;
+			if(k < x) temp-> rptr = 0;
+			else temp-> lptr = 0;
+			free(ptr);
+		}
+		return(x);
+	}else{
+		printf("NOT FOUND\n");
+		return 0;
+	}
 }
 int main(){
-	node *rut = root(100);
+	rut = root(100);
 	insert(95,rut);
 	insert(45,rut);
 	insert(195,rut);
@@ -96,5 +145,7 @@ int main(){
 	printf("%d\n",search(95,rut)->data);
 	printf("%d\n",lchild(100,rut)->data);
 	printf("%d\n",rchild(100,rut)->data);
+	printf("%d\n",delete(100,rut));
+	preorder(rut);
 	return 0;
 }
